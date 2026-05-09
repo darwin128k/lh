576a229335e6e8e2fa45fb78ba5682e4f127903a
Egor Tsyganchuk <darwin128K@gmail.com>
2026-05-09 10:11:37 +0300

    Remove extra space in slice overlap check function

--- src/lh/memory/bounds/slice.c
+++ src/lh/memory/bounds/slice.c
@@ -381,7 +381,6 @@ lh_memory_bounds_slice_overlaps_v(const lh_memory_bounds_slice_t *self,
                                   const lh_memory_bounds_slice_t *other)
 {
     lh_runtime_check(lh_memory_bounds_slice_is_valid(other), lh_runtime_error_code_invalid_range);
-
     return lh_memory_bounds_slice_overlaps(self, other);
 }
 
