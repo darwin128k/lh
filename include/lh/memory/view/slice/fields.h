67cc9148a3dcb623d6b3edfc9ff79a72541e095b
Egor Tsyganchuk <darwin128K@gmail.com>
2026-05-13 08:26:22 +0300

    Add documentation for macro utilities in fields.h and arg.h

--- include/lh/memory/view/slice/fields.h
+++ include/lh/memory/view/slice/fields.h
@@ -1,8 +1,23 @@
+/**
+ * @file fields.h
+ * @brief Field macro for read-only slice view types.
+ *
+ * Defines a macro to generate const-qualified slice field declarations using
+ * interval bounds fields with pointer element type.
+ */
+
 #ifndef LH_MEMORY_VIEW_SLICE_FIELDS_H
 #define LH_MEMORY_VIEW_SLICE_FIELDS_H
 
 #include <lh/interval/bounds/fields.h>
 
+/**
+ * @def lh_memory_view_slice_fields(T)
+ * @brief Generate read-only slice fields for a given element type.
+ *
+ * @param T Element type (e.g., lh_void, lh_char_t).
+ * Expands to interval bounds fields with const pointer element type.
+ */
 #define lh_memory_view_slice_fields(T) lh_interval_bounds_fields(const T *)
 
 #endif // LH_MEMORY_VIEW_SLICE_FIELDS_H
--- include/lh/util/arg.h
+++ include/lh/util/arg.h
@@ -6,10 +6,37 @@
 #ifndef LH_UTIL_ARG_H
 #define LH_UTIL_ARG_H
 
+/**
+ * @def lh_arg_concat_x(a, b)
+ * @brief Concatenate two preprocessing tokens without expanding arguments first.
+ *
+ * @param a Left token.
+ * @param b Right token.
+ */
 #define lh_arg_concat_x(a, b) a##b
+
+/**
+ * @def lh_arg_concat(a, b)
+ * @brief Concatenate two preprocessing tokens after macro argument expansion.
+ *
+ * @param a Left token.
+ * @param b Right token.
+ */
 #define lh_arg_concat(a, b) lh_arg_concat_x(a, b)
 
+/**
+ * @def lh_arg_get_count_x(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...)
+ * @brief Implementation helper for ::lh_arg_get_count.
+ */
 #define lh_arg_get_count_x(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
+
+/**
+ * @def lh_arg_get_count(...)
+ * @brief Count macro arguments in the range 1..10.
+ *
+ * Expands to the number of supplied arguments. Empty argument lists are not
+ * supported.
+ */
 #define lh_arg_get_count(...) lh_arg_get_count_x(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
 
 #endif // LH_UTIL_ARG_H
