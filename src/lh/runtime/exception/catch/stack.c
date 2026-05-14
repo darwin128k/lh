abcd9b3994a9afedbe6cce39b51a25514c06218b
Egor Tsyganchuk <darwin128K@gmail.com>
2026-05-14 12:38:31 +0300

    Reorder include statements for consistency in stack.c

--- src/lh/runtime/exception/catch/stack.c
+++ src/lh/runtime/exception/catch/stack.c
@@ -1,10 +1,11 @@
 #include <lh/runtime/exception/catch/stack.h>
-#include <lh/assert/static.h>
-#include <lh/library/fallback.h>
-#include <lh/compiler/destructor.h>
-#include <lh/compiler/constructor.h>
 #include <lh/attribute/thread_local.h>
+#include <lh/compiler/constructor.h>
+#include <lh/compiler/destructor.h>
+#include <lh/library/fallback.h>
+#include <lh/assert/static.h>
 #include <lh/util/addr.h>
+#include <lh/size.h>
 #include <lh/null.h>
 
 lh_assert_static(LH_LIBRARY_OPTION_RUNTIME_EXCEPTION_CATCH_STACK_MAX,
