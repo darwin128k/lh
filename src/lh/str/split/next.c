e0835f39650c0f2bff011b91957eeefff8c8c353
Egor Tsyganchuk <darwin128k@gmail.com>
2026-09-20 12:47:43 +0300

    Refactor string splitting functions to eliminate redundant emit function and streamline logic

--- src/lh/str/split/next.c
+++ src/lh/str/split/next.c
@@ -6,11 +6,26 @@
 #include <lh/util/addr.h>
 #include <lh/util/str/ptr.h>
 
-static lh_bool_t
-lh_str_ptr_split_emit(lh_str_cptr str, lh_usize_t str_size, lh_usize_t start, lh_str_cptr delim_at,
-                      lh_usize_t *pos, lh_str_cptr *field, lh_usize_t *field_size,
-                      lh_bool_t *had_delim)
+lh_bool_t
+lh_str_ptr_split_next(lh_str_cptr str, lh_usize_t str_size, lh_char_t delim, lh_usize_t *pos,
+                      lh_str_cptr *field, lh_usize_t *field_size, lh_bool_t *had_delim)
 {
+    lh_usize_t start;
+    lh_str_cptr delim_at;
+
+    lh_assert_runtime_ref(str);
+    lh_assert_runtime_ref(pos);
+    lh_assert_runtime_ref(field);
+    lh_assert_runtime_ref(field_size);
+    lh_assert_runtime_ref(had_delim);
+
+    start = *pos;
+    if (start >= str_size)
+    {
+        return lh_bool_false;
+    }
+
+    delim_at = lh_str_ptr_find_of_char(str + start, str_size - start, delim);
     *field = str + start;
     if (delim_at != lh_null)
     {
@@ -25,29 +40,6 @@ lh_str_ptr_split_emit(lh_str_cptr str, lh_usize_t str_size, lh_usize_t start, lh
         *pos = str_size;
     }
     return lh_bool_true;
-}
-
-lh_bool_t
-lh_str_ptr_split_next(lh_str_cptr str, lh_usize_t str_size, lh_char_t delim, lh_usize_t *pos,
-                      lh_str_cptr *field, lh_usize_t *field_size, lh_bool_t *had_delim)
-{
-    lh_usize_t start;
-
-    lh_assert_runtime_ref(str);
-    lh_assert_runtime_ref(pos);
-    lh_assert_runtime_ref(field);
-    lh_assert_runtime_ref(field_size);
-    lh_assert_runtime_ref(had_delim);
-
-    start = *pos;
-    if (start >= str_size)
-    {
-        return lh_bool_false;
-    }
-
-    return lh_str_ptr_split_emit(str, str_size, start,
-                                 lh_str_ptr_find_of_char(str + start, str_size - start, delim), pos,
-                                 field, field_size, had_delim);
 }
 
 lh_bool_t
@@ -82,8 +74,20 @@ lh_str_ptr_split_next_if(lh_str_cptr str, lh_usize_t str_size, lh_str_ptr_split_
         }
     }
 
-    return lh_str_ptr_split_emit(str, str_size, start, delim_at, pos, field, field_size,
-                                 had_delim);
+    *field = str + start;
+    if (delim_at != lh_null)
+    {
+        *field_size = lh_cast_static(lh_usize_t, delim_at - (str + start));
+        *had_delim = lh_bool_true;
+        *pos = start + *field_size + 1U;
+    }
+    else
+    {
+        *field_size = str_size - start;
+        *had_delim = lh_bool_false;
+        *pos = str_size;
+    }
+    return lh_bool_true;
 }
 
 lh_bool_t
